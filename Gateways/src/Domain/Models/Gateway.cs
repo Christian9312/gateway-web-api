99ce99f424b537c5a99a07648aec1be712ee11a7
christian9312 <chrisdd9312@gmail.com>
2020-01-19 17:16:40 -0500

    add models

---
+++ Gateways/src/Domain/Models/Gateway.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Gateways.Domain
+{
+    public class Gateway
+    {
+        public string SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public IEnumerable<Peripheral> AssociatedPeripherals { get; set; }
+
+    }
+}
