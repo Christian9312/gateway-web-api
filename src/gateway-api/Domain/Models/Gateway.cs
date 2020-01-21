18c3186408d260862cba2df5fd37d695cc1171d2
christian9312 <chrisdd9312@gmail.com>
2020-01-21 17:55:16 -0500

    move project to src folder

--- src/gateway-api/Domain/Models/Gateway.cs
+++ src/gateway-api/Domain/Models/Gateway.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Threading.Tasks;
@@ -8,13 +9,14 @@ namespace Gateways.Domain
 {
     public class Gateway
     {
+       
         public string SerialNumber { get; set; }
 
         public string Name { get; set; }
 
         public string Address { get; set; }
 
-        public IEnumerable<Peripheral> AssociatedPeripherals { get; set; }
+        public IList<Peripheral> AssociatedPeripherals { get; set; } = new List<Peripheral>();
 
     }
 }
---
+++ src/gateway-api/Domain/Models/Peripheral.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Gateways.Domain
+{
+    public class Peripheral
+    {
+        [Required(ErrorMessage = "UID is required")]
+        public int UId { get; set; }
+
+        [StringLength(60, ErrorMessage = "Vendor can't be longer than 60 characters")]
+        public string Vendor { get; set; }
+
+        [Required(ErrorMessage = "Date created is required")]
+        public DateTimeOffset CreationDate { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public PeripheralStatus Status { get; set; }
+
+        public string GatewayId { get; set; }
+        public Gateway Gateway { get; set; }
+
+
+    }
+
+    public enum PeripheralStatus
+    {
+        Offline,
+        Online
+    }
+}
