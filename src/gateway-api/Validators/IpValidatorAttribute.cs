e8b34436cdcff9affeeff01a81711bd53867d1a5
christian9312 <chrisdd9312@gmail.com>
2020-01-22 13:35:06 -0500

    add ip validator

---
+++ src/gateway-api/Validators/IpValidatorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Gateways.Validators
+{
+    public class IpValidatorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is string ip))
+                return new ValidationResult("Invalid address value");
+
+            var isValid = IPAddress.TryParse(ip, out _);
+
+            return isValid ? ValidationResult.Success : new ValidationResult("Invalid address value");
+        }
+    }
+}
